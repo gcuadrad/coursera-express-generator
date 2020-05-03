f607c7662375ee2f2d8899feccdccc131ef960da
Gonzalo Cuadra <gonzalocuadra@protonmail.com>
2020-05-03 01:49:42 -0400

    assigment 4

---
+++ models/favorites.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const favoriteSchema = new Schema({
+  dishes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Dish'
+  }],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Favorite', favoriteSchema);
